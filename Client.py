dd214901e8f8c9fcd15e4c1a12a8815c312821d8
OrShoshani <or.shoshani27@gmail.com>
2020-12-29 17:49:03 +0200

    server pah

--- Client.py
+++ Client.py
@@ -1,16 +1,33 @@
 import socket
+import struct
 
+PORT = 13117
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+TCPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+UDPClientSocket.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
 
-SERVER = "127.0.0.1"
-PORT = 8080
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((SERVER, PORT))
-client.sendall(bytes("This is from Client",'UTF-8'))
+print("Client started, listening for offer requests...")
+UDPClientSocket.bind(("",PORT))
+
 while True:
-  in_data =  client.recv(1024)
-  print("From Server :" ,in_data.decode())
-  out_data = input()
-  client.sendall(bytes(out_data,'UTF-8'))
-  if out_data=='bye':
-      break
-client.close()
+  message,addr = UDPClientSocket.recvfrom(1024)
+  if message:
+    print("yes")
+  print(struct.unpack("QQQ",message))
+  
+  
+  
+  
+
+  
+
+# client.connect((SERVER, PORT))
+# client.sendall(bytes("This is from Client",'UTF-8'))
+# while True:
+  
+#   print("From Server :" ,in_data.decode())
+#   out_data = input()
+#   client.sendall(bytes(out_data,'UTF-8'))
+#   if out_data=='bye':
+#       break
+# client.close()
--- Server.py
+++ Server.py
@@ -1,14 +1,24 @@
-import socket, threading
+import socket
+import time
+import threading
+import struct
 
+def send_thread_interval():
+    threading.Timer(interval=1.0,function=sendBraodcast).start()
+
+def sendBraodcast():
+    message = struct.pack("Ibh", 0xfeedbeef, 0x2,PORT)
+    UDPServerSocket.sendto(message,("<broadcast>",13117))
+    
 class ClientThread(threading.Thread):
 
     def __init__(self,clientAddress,clientsocket):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
         print ("New connection added: ", clientAddress)
+
     def run(self):
         print ("Connection from : ", clientAddress)
-        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
         msg = ''
         while True:
             data = self.csocket.recv(2048)
@@ -19,14 +29,20 @@ class ClientThread(threading.Thread):
             self.csocket.send(bytes(msg,'UTF-8'))
         print ("Client at ", clientAddress , " disconnected...")
 
-LOCALHOST = "127.0.0.1"
-PORT = 8080
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((LOCALHOST, PORT))
-print("Server started, listening on IP address", LOCALHOST)
-while True:
-    server.listen(1)
-    clientsock, clientAddress = server.accept()
+HOST = "127.0.0.1"
+PORT   = 2027
+# Create a datagram socket
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+TCPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+# Bind to address and ip
+TCPServerSocket.bind((HOST, PORT))
+print("Server started,listening on IP address",HOST)
+UDPServerSocket.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
+send_thread_interval()
+
+# Listen for incoming datagrams
+while(True):
+    TCPServerSocket.listen(1)
+    clientsock, clientAddress = TCPServerSocket.accept()
     newthread = ClientThread(clientAddress, clientsock)
     newthread.start()
