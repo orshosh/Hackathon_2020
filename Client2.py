7e6c73d8e7cf64323df6c220272c2e7c6b330d35
OrShoshani <or.shoshani27@gmail.com>
2020-12-30 17:28:07 +0200

    until keboard

--- Client2.py
+++ Client2.py
@@ -8,15 +8,15 @@ def send_group_name():
   group_name = "slow-start\n"
   TCPClientSocket.sendto(group_name.encode('UTF-8','strict'),addr)
 
-def start_game():
-  while True:
-    print("instart_game")
-    # tty.setcbreak(sys.stdin.fileno())
-    char=sys.stdin.read(1)
-    TCPClientSocket.sendto(char.encode(('UTF-8','strict'),addr))
-    flag_to_stop,adress = TCPClientSocket.recvfrom(1024)
-    if flag_to_stop.decode('UTF-8','strict') == False:
-      break
+# def start_game():
+#   while True:
+#     print("instart_game")
+#     tty.setcbreak(sys.stdin.fileno())
+#     char=sys.stdin.read(1)
+#     TCPClientSocket.sendto(char.encode(('UTF-8','strict'),addr))
+#     flag_to_stop,adress = TCPClientSocket.recvfrom(1024)
+#     if flag_to_stop.decode('UTF-8','strict') == False:
+#       break
 
 def TCPconnect_server(port,source):
   TCPClientSocket.connect((source[0], values_message[2]))
@@ -27,7 +27,7 @@ def TCPconnect_server(port,source):
     try:
       if not message is None:
         print(message.decode('UTF-8','strict'))
-        start_game()
+        # start_game()
     except:
       continue
 
