dd60e729c969968289f2753d94b4e4e18fb78d08
Guygool <64363416+Guygool@users.noreply.github.com>
2020-12-31 14:41:06 +0200

    added comments

--- Client.py
+++ Client.py
@@ -7,6 +7,7 @@ def send_group_name():
   group_name = "gogogo\n"
   TCPClientSocket.sendto(group_name.encode('UTF-8','strict'),addr)
 
+# sends a message to server via the open TCP connection for every key press.
 def start_game():
   print("instart_game")
   while True:
@@ -16,7 +17,8 @@ def start_game():
         TCPClientSocket.sendall(send_key)
         print(key)
 
-
+#sends the group name back to server via "send_group_name()"
+#wait for game start message to return and then starts playing 
 def TCPconnect_server(port,source):
   print(source[0],port)
   TCPClientSocket.connect((source[0], port))
@@ -33,24 +35,27 @@ def TCPconnect_server(port,source):
       continue
     
 
+buffer_size = 1024
+PORT = 13117 #hard coded for hackathon porpous.
 
-PORT = 13117
+#client has both TCP and UDP sockets
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 TCPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 UDPClientSocket.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
 
 print("Client started, listening for offer requests...")
-UDPClientSocket.bind(('',PORT))
+UDPClientSocket.bind(('',PORT)) #bind all incoming packets to hackathon port
 values_message = None
 addr = None
 while True:
-    TCPClientSocket.settimeout(0.2)
-    message,addr = UDPClientSocket.recvfrom(1024)
-    values_message = struct.unpack("Ibh",message)
+    TCPClientSocket.settimeout(0.2) #set time to avoid "busy wait"
+    message,addr = UDPClientSocket.recvfrom(buffer_size)
+    #translate and validate message
+    values_message = struct.unpack("Ibh",message) 
     if (not message is None) and (hex(values_message[0]) == '0xfeedbeef') and (hex(values_message[1]) == '0x2') :
       break
 
-UDPClientSocket.close()
+UDPClientSocket.close() #no need to keep listening to UDP port while game is in play
 TCPconnect_server(port=2027,source=addr)
 
 
--- Server.py
+++ Server.py
@@ -11,14 +11,18 @@ Group2 = []
 PLAYERS = {}
 Group1_counter = 0
 Group2_counter = 0
-  
+ buffer_size = 1024  
 
+
+
+#send a broadcast every set time.
 def send_thread_interval():
     global kill_thread
     if kill_thread:
         threading.Timer(interval=1.0,function=send_thread_interval).start()
         sendBraodcast()
-       
+
+# build the message and sent to all available using UDP socket boradcast     
 def sendBraodcast():
     print("send")
     message = struct.pack("Ibh", 0xfeedbeef, 0x2,PORT)
@@ -30,7 +34,7 @@ def get_winner():
     else:
         return Group2,"Group 2"
 
-
+#welcome msg
 def write_msg():
     game_message = "Welcome to Keyboard Spamming Battle Royale.\nGroup 1:\n==\n"
     for player in Group1:
@@ -42,6 +46,8 @@ def write_msg():
     game_message += "\nStart pressing keys on your keyboard as fast as you can!!"
     return game_message
 
+
+#Function recive a group name and count, updates the relevant score
 def insret_count(count,client_name):
     if client_name in Group1:
         global Group1_counter
@@ -51,6 +57,19 @@ def insret_count(count,client_name):
         Group2_counter += count
     
 
+ # Once the server finished collecting all players, initialize a game thread for each player
+ # by calling  the "run_game()" method.
+ # after all players returnd, finish the game.   
+def start_game():
+    for key in PLAYERS:
+        game_thread = threading.Thread(target=run_game,args=(PLAYERS[key][0],PLAYERS[key][1],key))
+        game_thread.start()
+    game_thread.join()
+    finish_game()
+
+
+ #Function send the game message to the client via the relevant socket
+ # server recives packets for 10 seconds, then updates the count for each group via "insert_count()"
 def run_game(client_socket,client_address,client_name):
     game_message = write_msg()
     client_socket.sendto(game_message.encode('UTF-8','strict'),client_address)
@@ -58,22 +77,14 @@ def run_game(client_socket,client_address,client_name):
     count_char = 0
     while time.time() - start_time < 10:
         try:
-            char = client_socket.recv(1024)
+            char = client_socket.recv(buffer_size)
             if not char is None:
                 count_char +=1
         except:
             pass
     insret_count(count_char,client_name)
-    
-    
-def start_game():
-    for key in PLAYERS:
-        game_thread = threading.Thread(target=run_game,args=(PLAYERS[key][0],PLAYERS[key][1],key))
-        game_thread.start()
-    game_thread.join()
-    finish_game()
-    
-
+       
+#when game is finished, the function creates the final message and sends it to each player
 def finish_game():
     winner_list, winner = get_winner()
     message = final_msg(winner_list,winner)
@@ -84,7 +95,7 @@ def finish_game():
         print("Game over, sending out offer requests...")
         client_socket.close()
 
-
+# modular funtion to build final message according to the winner
 def final_msg(winner_list, winner):
     global Group1_counter
     global Group2_counter
@@ -99,8 +110,11 @@ def final_msg(winner_list, winner):
         final_message += group
     return final_message 
 
-class ClientThread(threading.Thread):
 
+##the server creates a thread for each client it is communicating with
+#this class makes holds all clientTreads methods
+class ClientThread(threading.Thread):
+    #each client has a unique address and socket to communicate with
     def __init__(self,clientAddress,clientsocket):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
@@ -118,8 +132,9 @@ class ClientThread(threading.Thread):
         
 
 hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
+local_ip = socket.gethostbyname(hostname) #ip of local machine running the code
 PORT = 2027
+#creating needed sockets for server
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 TCPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 TCPServerSocket.bind(('', PORT))
@@ -132,12 +147,12 @@ broadcast_thread = threading.Thread(target=send_thread_interval)
 broadcast_thread.start()
 
 tmp_timp = time.time()
-while len(PLAYERS) == 0:
-    while time.time() - tmp_timp < 10:  
+while len(PLAYERS) == 0: #wait for at least one player to join game.
+    while time.time() - tmp_timp < 10:  #when 10 seconds are finished , the game will start
         try:
             TCPServerSocket.settimeout(0.5)
             clientsock, clientAddress = TCPServerSocket.accept()
-            newthread = ClientThread(clientAddress, clientsock)
+            newthread = ClientThread(clientAddress, clientsock)#new thread foe each client connected
             newthread.start()
         except:
             continue
